7d723ae5e0db437915211a70e3c3f4851e054afc
Lao Thao <lao793@stanford.edu>
2012-10-29 18:17:34 -0700

    connected to db

--- submit.php
+++ submit.php
@@ -1,28 +1,65 @@
 <!DOCTYPE html>
 <html>
-	<head>
+<head>
 
-	</head>
+</head>
 
-	<body>
+<body>
+	<div>
+	<?php
+		include("config.php");
+		$what = $_POST["activity"];
+		$how = $_POST["rating"];
+		$when = $_POST["time"];
+		$notes = $_POST["notes"];
 		
-		<div>
-			<?php
-			
-			// Take in parameters
-			$what = $_POST["activity"];
-			$how = $_POST["rating"];
-			$ctime = $_POST["time"];
-			$notes = $_POST["notes"];
-			
-			// Insert into orders
-			// but oops query is not defined... yet
-			//INSERT INTO `c_cs147_lao793`.`orders` (`name`, `email`, `book`) VALUES ('$name', '$email', '$book');
-			
-			$query = "INSERT INTO `c_cs147_sharonxh`.`clarity_username` (`what`, `how`, `ctime`, 'notes') VALUES ('$what', '$how', '$ctime', '$notes')";
-			
-			$result = mysql_query($query);			
-			?>
-		</div>
-	</body>
+		//INSERT INTO `c_cs147_lao793`.`orders` (`name`, `email`, `book`) VALUES ('$name', '$email', '$book');
+		
+		$query = "INSERT INTO `c_cs147_lao793`.`clarity` (`what`, `how`, `when`, `notes`) VALUES ('$what', '$how', '$when', '$notes')";
+		echo $query;
+		$result = mysql_query($query);
+		echo "<p>Submitted</p>";
+		
+		?>
+	</div>
+	<script type="text/javascript">
+	</script>
+</body>
 </html>
+
+
+
+
+
+
+
+
+
+
+<!--
+<title>Maya Online Books</title>
+<link rel="apple-touch-icon" href="appicon.png" />
+<link rel="apple-touch-startup-image" href="startup.png">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<meta name="viewport" content="width=device-width, user-scalable=no" />
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+
+<link href="style.css" rel="stylesheet" type="text/css">
+-->
+
+<!--
+<script type="text/javascript">
+$("a").click(function (event) {
+    event.preventDefault();
+    window.location = $(this).attr("href");
+});
+$("#someform").submit(function() {
+	event.preventDefault();
+	$event.preventDefault();
+	$.post("submit.php", $("someform").serialize(), function(data) {
+		$result("result").html(data);
+	});
+});
+</script>
+-->
