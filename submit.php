1bdf90b11b90931b65be36e379da9b2dfd390122
Lao Thao <lao793@stanford.edu>
2012-10-29 20:50:41 -0700

    added more submit functionality and a page for it

--- submit.php
+++ submit.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-
+    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Clarity</title>
+    <link href="css/style.css"rel="stylesheet" type="text/css" />
 </head>
 
 <body>
+	<div class="nav2">
+		<p>Submitted</p>
+    </div>
 	<div>
 	<?php
 		include("config.php");
+		//echo $_POST['rating'];
 		$what = $_POST["activity"];
 		$how = $_POST["rating"];
 		$when = $_POST["time"];
@@ -42,11 +49,20 @@
 		$query = "INSERT INTO `c_cs147_lao793`.`clarity` (`what`, `how`, `when`, `notes`, `tag1`, `tag2`, `tag3`, `tag4`, `tag5`, `tag6`) VALUES ('$what', '$how', '$when', '$notes', '$tag1', '$tag2', '$tag3', '$tag4', '$tag5', '$tag6')";
 		//echo $query;
 		$result = mysql_query($query);
-		//echo "<p>Submitted</p>";
 		
 		?>
 	</div>
 	<script type="text/javascript">
 	</script>
+	<div id="footer">
+		<div class="box">
+		<img src="images/Ribbon.png" height="64" style ="max-width: 320px" alt="Whole Ribbon" usemap="#ribbonMap" align ="center">
+		<map name="ribbonMap">
+		<area shape = "rect" coords="0,0,64,64" alt="Progress" href="progress.html"> </area>
+		<area shape = "rect" coords="64,0,128,64" alt="Log Activity" href="log.html">	
+		<area shape = "rect" coords="128,0,192,64" alt="Log Activity" href="history.html">
+		<area shape = "rect" coords="192,0,256,64" alt="Log Activity" href="recommendations.html">
+		<area shape = "rect" coords="256,0,320,64" alt="Log Activity" href="help.html">
+	</div>
 </body>
 </html>
